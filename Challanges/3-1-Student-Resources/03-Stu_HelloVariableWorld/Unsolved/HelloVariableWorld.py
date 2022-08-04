b9a5b519622d136fe07d1df789dedba01151dab1
Jacob LaBure <jlabure@gmail.com>
2022-08-04 11:41:11 -0600

    merica

---
+++ Challanges/3-1-Student-Resources/03-Stu_HelloVariableWorld/Unsolved/HelloVariableWorld.py
@@ -0,0 +1,44 @@
+# Create a variable called 'name' that holds a string
+Name = "Bob"
+
+
+# Create a variable called 'country' that holds a string
+
+Country = France
+
+
+# Create a variable called 'age' that holds an integer
+
+Age = '50'
+
+# Create a variable called 'hourly_wage' that holds an integer
+
+hourly_wage = 30
+
+
+# Calculate the daily wage for the user
+
+daily_wage = (hourly wage * 8)
+
+
+# Create a variable called 'satisfied' that holds a boolean
+
+Satisfied = True
+
+
+# Print out "Hello <name>!"
+
+"Hello <name>"
+
+
+# Print out what country the user entered
+
+
+# Print out the user's age
+
+
+# With an f-string, print out the daily wage that was calculated
+
+
+# With an f-string, print out whether the users were satisfied
+
